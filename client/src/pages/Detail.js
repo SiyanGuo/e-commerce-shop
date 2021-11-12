521779e9cf4eb2684e149636954d3d59193883f4
Siyan Guo <siyang@Hey-There.local>
2021-11-11 20:54:32 -0500

    Implement Updating and Deleting Items

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -15,11 +15,11 @@ import Cart from '../components/Cart';
 
 function Detail() {
   const [state, dispatch] = useStoreContext();
+  const { products, cart } = state;
   const { id } = useParams();
   const [currentProduct, setCurrentProduct] = useState({});
   // const products = data?.products || [];
   const { loading, data } = useQuery(QUERY_PRODUCTS);
-  const { products } = state;
 
   // useEffect(() => {
   //   if (products.length) {
@@ -28,11 +28,29 @@ function Detail() {
   // }, [products, id]);
 
   const addToCart = () => {
+    const itemInCart = cart.find((cartItem) => cartItem._id === id);
+
+    if (itemInCart) {
+      dispatch({
+        type: UPDATE_CART_QUANTITY,
+        _id: id,
+        purchaseQuantity: parseInt(itemInCart.purchaseQuantity) + 1
+      });
+    } else {
+      dispatch({
+        type: ADD_TO_CART,
+        product: { ...currentProduct, purchaseQuantity: 1 }
+      });
+    }
+  };
+
+  const removeFromCart = () => {
     dispatch({
-      type: ADD_TO_CART,
-      product: { ...currentProduct, purchaseQuantity: 1 }
+      type: REMOVE_FROM_CART,
+      _id: currentProduct._id
     });
   };
+
 
   useEffect(() => {
     if (products.length) {
@@ -58,7 +76,12 @@ function Detail() {
           <p>
             <strong>Price:</strong>${currentProduct.price}{' '}
             <button onClick={addToCart}>Add to Cart</button>
-            <button>Remove from Cart</button>
+            <button
+              disabled={!cart.find(p => p._id === currentProduct._id)}
+              onClick={removeFromCart}
+            >
+              Remove from Cart
+            </button>
           </p>
 
           <img
@@ -68,7 +91,7 @@ function Detail() {
         </div>
       ) : null}
       {loading ? <img src={spinner} alt="loading" /> : null}
-    <Cart />
+      <Cart />
     </>
   );
 }
